98cbbce83d151528179517c707cde73211ae588a
Joe <hutcheson2001@yahoo.com>
2022-06-21 18:05:34 -0400

    commit tree 56978d62c33894dd17150b7cb98e119eca05b89f

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,23 @@
 require_relative './stack'
+require "pry"
 
-# your code here
+class MyQueue
+  attr_accessor :s1, :s2
+  
+  def initialize
+    @s1 = Stack.new
+    @s2 = Stack.new
+  end
+
+  def add(value)
+    # @s1.push(value)
+    # @s2.push(@s1.pop)
+    binding.pry
+  end
+
+  def remove
+    return @s2.pop
+  end
+
+  
+end
